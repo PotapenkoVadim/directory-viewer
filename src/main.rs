6a94dff692042f15764feb94d7a544165ba7ab60
Potapenko Vadim <vpotapenko@studio-tg.ru>
2023-05-02 21:39:16 +0300

    feat: implement directory viewer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{fs, env, io::ErrorKind};
+
 fn main() {
-    
+    let current_dir = "./";
+    let not_found_message = "WARNING: The path does not exist";
+
+    let args: Vec<String> = env::args()
+        .skip(1)
+        .collect();
+
+    let verifiable_path = match args.first() {
+        Some(path) => path,
+        None => current_dir
+    };
+
+    match fs::metadata(verifiable_path) {
+        Ok(_) => {
+            let mut directories: Vec<String> = Vec::new();
+            let paths = fs::read_dir(verifiable_path).unwrap();
+
+            for path in paths {
+                let document = path
+                    .unwrap()
+                    .path()
+                    .display()
+                    .to_string()
+                    .replace(current_dir, "");
+
+                directories.push(document);
+            }
+
+            println!("{}", directories.join("   "));
+        },
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                println!("{not_found_message}");
+            } else {
+                panic!("Unexpected error: {:?}", e);
+            }
+        }
+    }
 }
