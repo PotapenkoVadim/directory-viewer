03758ded4faa325c8848678d282a2fde9026e14e
Potapenko Vadim <vpotapenko@studio-tg.ru>
2023-06-01 19:19:42 +0300

    fix: fix file display

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use std::{fs, env, io::ErrorKind};
 
 fn main() {
-    let current_dir = "./";
-    let not_found_message = "WARNING: The path does not exist";
-
     let args: Vec<String> = env::args()
         .skip(1)
         .collect();
 
     let verifiable_path = match args.first() {
         Some(path) => path,
-        None => current_dir
+        None => "./"
     };
 
     match fs::metadata(verifiable_path) {
@@ -21,10 +18,9 @@ fn main() {
             for path in paths {
                 let document = path
                     .unwrap()
-                    .path()
-                    .display()
-                    .to_string()
-                    .replace(current_dir, "");
+                    .file_name()
+                    .into_string()
+                    .unwrap();
 
                 directories.push(document);
             }
@@ -33,7 +29,7 @@ fn main() {
         },
         Err(e) => {
             if e.kind() == ErrorKind::NotFound {
-                println!("{not_found_message}");
+                println!("WARNING: The path does not exist");
             } else {
                 panic!("Unexpected error: {:?}", e);
             }
